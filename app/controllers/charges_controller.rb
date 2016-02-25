85bbd611283b3f047c9f9474b3e708a83eb1da3a
Chris Dorman <chrisdorman1978@gmail.com>
2016-02-25 18:49:25 +0000

    added the model and purchases controller and done a db migrate abd db drop

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,21 +1,25 @@
 class ChargesController < ApplicationController
 
 	def create
-	  # Amount in cents
-	  @amount = 500
-
+	  
 	  customer = Stripe::Customer.create(
-	    :email => 'example@stripe.com',
+	    :email => params[:stripeEmail],
 	    :card  => params[:stripeToken]
 	  )
 
 	  charge = Stripe::Charge.create(
 	    :customer    => customer.id,
-	    :amount      => @amount,
-	    :description => 'Rails Stripe customer',
+	    :amount      => params[:amount],
+	    :description => 'Placenta wash',
 	    :currency    => 'usd'
 	  )
 
+	  purchase = Purchase.create(email: params[:stripeEmail], card: params[:stripeToken], 
+	      amount: params[:amount], description: charge.description, currency: charge.currency,
+	      customer_id: customer.id, product_id: 1)
+
+	  redirect_to purchase
+	  
 	rescue Stripe::CardError => e
 	  flash[:error] = e.message
 	  redirect_to charges_path
