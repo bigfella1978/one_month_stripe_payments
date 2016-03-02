e9610abb3a7ca3b5645058a76a571b2004afeca2
Chris Dorman <chrisdorman1978@gmail.com>
2016-03-02 19:36:42 +0000

    potential issue with email uniqueness

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') unless AdminUser.find_by(email: 'admin@example.com')
 
 placenta_wash = Product.create(title: "Placenta Wash", 
  subtitle: "Nippon Beauty", author: "Chris Dorman", 
